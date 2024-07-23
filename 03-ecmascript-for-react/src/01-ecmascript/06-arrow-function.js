f6126d1297ee6649b8a2625c29a1f9dc89464caf
happyhye <50566@naver.com>
2024-07-23 09:18:39 +0900

    [학습] 화살표 함수 표현식

--- 03-ecmascript-for-react/src/01-ecmascript/06-arrow-function.js
+++ 03-ecmascript-for-react/src/01-ecmascript/06-arrow-function.js
@@ -3,7 +3,11 @@
 
 // 🔶 일반 함수 선언을 화살표 함수 표현식으로 변경합니다.
 // 참고: https://mzl.la/3Uej8M3
-function currencyKR(value) {
+
+// Coffeescript ->
+// ECMAScript =>
+
+const currencyKR = (value) => {
   if (typeof value !== 'number' || Number.isNaN(value)) {
     throw new Error('currencyKR 함수에는 숫자 값만 전달해야 합니다.');
   }
@@ -12,9 +16,10 @@ function currencyKR(value) {
 }
 
 // 🔶 일반 함수 선언을 화살표 함수 표현식으로 변경합니다.
-function numberWithComma(value) {
-  return value.toString().replace?.(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-}
+// 매개변수가 하나만 존재할 경우, 괄호 생략 가능
+const numberWithComma = (value) => value.toString().replace?.(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+
+
 
 function run() {
   let price = currencyKR(2_107_000);
