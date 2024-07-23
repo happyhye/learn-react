ac080ea7e6571dcdcb6f0b3d97d5752163cc0ab3
happyhye <50566@naver.com>
2024-07-23 09:35:19 +0900

    [학습] 나머지 매개변수

--- 03-ecmascript-for-react/src/01-ecmascript/08-rest-parameters.js
+++ 03-ecmascript-for-react/src/01-ecmascript/08-rest-parameters.js
@@ -2,16 +2,19 @@
 // rest parameters
 
 function _sum() {
-  const numbers = Array.from(arguments);
+  const numbers = Array.from(arguments); // arguments는 유사배열
   return numbers.reduce((result, number) => result + number, 0);
+  // reduce에 전달되는 함수 이름: reducer function
 }
 
 // 🔶 나머지 매개변수를 사용해 sum 함수 코드 로직을 다시 작성합니다.
 // 참고: https://mzl.la/43Ro9yp
-const sum = () => {};
+const sum = (...numbers) => numbers.reduce((total, number) => total + number)
 
-let result1_1 = _sum(2, 3, 9, 12, 105);
+let result1_1 = _sum(2, 3, 9, 12, 105); // arguments
 let result1_2 = sum(2, 3, 9, 12, 105);
+console.log(result1_1);
+console.log(result1_2); //undefined
 console.log(Object.is(result1_1, result1_2));
 
 let result2_1 = _sum(90, 418, -7);
